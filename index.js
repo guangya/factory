5aaee66f89f7eb90066d9108cb13c07f553e3562
Anonymous <guangya@outlook.com>
2024-03-13 03:26:57 +0800

    为适应github pages调整文件结构

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 var app = express();
-app.use('/factory', express.static(path.join(__dirname, 'src')));
+app.use('/factory', express.static(__dirname));
 var server = app.listen('7002', () => {
     console.log('running');
 });
